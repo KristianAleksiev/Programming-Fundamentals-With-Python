18e3a2b2e7fbcd55573ef523af993a5f40975644
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-19 12:31:48 +0300

    Add files via upload

---
+++ 01.Basic_syntax_conditional_loops/even_numbers.py
@@ -0,0 +1,8 @@
+count = int(input())
+for _ in range(count):
+    current_number = int(input())
+    if current_number % 2 != 0:
+        print(f"{current_number} is odd!")
+        break
+else:
+    print("All numbers are even.")
