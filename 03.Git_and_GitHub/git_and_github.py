ed5f7c6d4fceabe5947981f265bde74877890c5e
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-23 20:44:14 +0300

    Add files via upload

---
+++ 03.Git_and_GitHub/git_and_github.py
@@ -0,0 +1,21 @@
+### Git and GitHub - Version control systems
+
+##Git - Source control system
+##Analysis(requirements) => Design(models) => Implementation(source code) => Build(scripts) => Testing => Release
+
+
+#Clone a Repo - Download a local copy of the remote project (Project source code)
+#git clone [remote url]
+
+#Commit a Changeset - Saves a set of changes locally
+#gil commit -m "[MESSAGE]"
+
+#Push the Changes - From local to Remote
+#Pull Changes - Synchronize the changes from Remote to local - Remote to local
+#git pull
+
+#Merge Changes - Merging changes from different people in one file
+
+## git, Git Bash, TortoiseGit
+
+## Branch - Main source - Combination of all branches (different features) - Managing conflicts
