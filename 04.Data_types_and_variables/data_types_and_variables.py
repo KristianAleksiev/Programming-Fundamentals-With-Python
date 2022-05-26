3081be71780adb9af6938a2a66970f6c1f9af2b4
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-26 15:42:03 +0300

    Add files via upload

---
+++ 04.Data_types_and_variables/data_types_and_variables.py
@@ -0,0 +1,32 @@
+### Data types and variables - Lab
+
+from decimal import Decimal
+
+a = Decimal("0.1")
+b = Decimal("0.1")
+c = Decimal("0.1")
+
+#" " = String literals
+
+abv = """de
+dsasdsadsda
+dsasadsadsad
+asd
+"""
+# Interpolation - "f{}"
+
+#Everything wiuthout a valueis False
+# number = None
+# if(number):
+#     print(number) => False
+
+#Chatbot library
+#from nltk.chat.util import Chat, reflections
+
+### SET
+
+#a = {"a", "b", "c", "b"} #= > Used for storing unique elements
+#print(a) => a , b, c => WITHOUT ORDER
+
+### None
+
