3a87bd12049f6bdd8d853c58cf68d097cf68991b
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-27 21:09:10 +0300

    Add files via upload

---
+++ 05.Exercise/elevator.py
@@ -0,0 +1,10 @@
+number_of_people = int(input())
+capacity = int(input())
+courses = 0
+
+if capacity != 0:
+    courses = number_of_people // capacity
+    if number_of_people % capacity != 0:
+        courses += 1
+
+print(courses)
