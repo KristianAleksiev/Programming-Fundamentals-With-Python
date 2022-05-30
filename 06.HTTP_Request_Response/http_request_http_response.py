a95b862d51c3d6c998ff0029967811728a33b185
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-30 20:35:48 +0300

    Add files via upload

---
+++ 06.HTTP_Request_Response/http_request_http_response.py
@@ -0,0 +1,18 @@
+# HTTP Protocol - Concepts - Text-based client-server protocol for the Internet - HyperText Transfer Protocol\
+## HTTP, TCP, IP, Ethernet
+
+# HTTP Developer tools
+# HTML Forms
+
+## Get request - Safe operation, easy to share, doesn't change anything on the server
+### Method, Headers, Body - CRUD
+### Method , URL, Protocol version
+
+## Post request - Sends data, doesn't record it in the URL, makes changes to the server
+
+# HTTP Response
+## Status code
+## Content type and disposition - Content-Type: text/html; charset=utf-8 # Encodedl with UTF-8 HTML Page
+
+# URL and URL Structure
+## Uniform resource locator - Protocol, host(IP)->DNS, port, path, query string(optional data), fragment
