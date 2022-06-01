8a4664f999c3508384658b7531ed5060dd5b87e7
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-01 21:17:01 +0300

    Delete 06.Lists_basics directory

--- 06.Lists_basics/courses.py
+++
@@ -1,8 +0,0 @@
-number_of_courses = int(input())
-courses_list = []
-
-for current_course in range(number_of_courses):
-    course_name = input()
-    courses_list.append(course_name)
-
-print(courses_list)
