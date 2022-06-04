dbb4b40e0dc73c3644011fb71ffb907acb63e596
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-04 18:16:37 +0300

    Add files via upload

---
+++ 08.Exercise/number_beggars.py
@@ -0,0 +1,18 @@
+sums_list = input().split(", ")
+count_of_beggars = int(input())
+final_list = []
+index_counter = 0
+# sums_list_as_digits = [int(i) for i in sums_list]
+sums_list_as_digits = []
+
+for element in sums_list:
+    sums_list_as_digits.append(int(element))
+
+while index_counter < count_of_beggars:
+    sum_for_current_beggar = 0
+    for element in range(index_counter, len(sums_list_as_digits), count_of_beggars):
+        sum_for_current_beggar += sums_list_as_digits[element]
+    index_counter += 1
+    final_list.append(sum_for_current_beggar)
+
+print(final_list)
