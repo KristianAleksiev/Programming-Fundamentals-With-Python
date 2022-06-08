0e62791ea7d7495b4986a8c5996fb091310aed26
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-08 21:25:08 +0300

    Add files via upload

---
+++ 10.Functions/repeat_string.py
@@ -0,0 +1,17 @@
+# result = lambda a, b: a * b
+#
+# string = input()
+# number = int(input())
+#
+# print(result(string, number))
+
+
+string = input()
+number = int(input())
+
+
+def repeat_string(a, b):
+    return a * b
+
+
+print(repeat_string(string, number))
