cd3e3d06839bb835d9ed17e4ba0cbfa482e598c8
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-06-10 21:41:00 +0300

    Add files via upload

---
+++ 11.Exercise/smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+def smallest_number(a: int, b: int, c: int):
+    min_integer = min(a, b, c)
+    print(min_integer)
+
+
+first_integer = int(input())
+second_integer = int(input())
+third_integer = int(input())
+
+smallest_number(first_integer, second_integer, third_integer)
