277d45450d078b246ade96b648f94c14cb41e7e4
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-04 21:00:55 +0300

    Add files via upload

---
+++ 12.Software_Development_Concepts/frontend_concept.py
@@ -0,0 +1,12 @@
+# Web front-end and DOM
+# AJAX and RESTful APIs - Asynchronous execution of HTTP requests from client-side JS, CRUD operations(create, read, update, delete)
+# Templating engines
+# Routing and routing libraries - "/"
+# Libraries vs. Frameworks -
+# Libraries - Piece of code which is imported for a certain need,
+# Framework - Uses our code(which uses libraries) to construct an app
+#
+# Mobile apps, desktop apps
+
+
+#JS front-end frameworks (React, Angular, Vue)
