40cfdd7874b12c04995f62640ee24e04de0cc2f0
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-04 21:00:37 +0300

    Add files via upload

---
+++ 12.Software_Development_Concepts/backend_concept.py
@@ -0,0 +1,8 @@
+# Server-side technologies
+# Data-management, ORM, logic - SQL - MySQL PostgreSQL   NoSQL - MongoDB
+# Python ORM - SQLAlchemy, Sequelize - JavaScript ORM
+# Model-View-Controller - Django, Ruby on Rails, Express
+# Backend frameworks -
+# Virtualization VM(server) and Cloud
+# Containers and Docker -
+# Microservices, containers and cloud
