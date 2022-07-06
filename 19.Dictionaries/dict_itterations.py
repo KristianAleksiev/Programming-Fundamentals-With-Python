1f4190a88e480873b052001e327c145173cd3952
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-06 21:43:19 +0300

    Add files via upload

---
+++ 19.Dictionaries/dict_itterations.py
@@ -0,0 +1,9 @@
+# Iterating through Dictionaries
+for key in dict.keys():
+    pass
+for value in dict.values():
+    pass
+for elements in dict.items():# Tuples
+    pass
+for key, value in dict.items():
+    pass
