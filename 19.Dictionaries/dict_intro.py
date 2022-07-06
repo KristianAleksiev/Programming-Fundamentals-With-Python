021792f64996d375fd6035fe59d44999c4650c7f
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-06 21:43:09 +0300

    Add files via upload

---
+++ 19.Dictionaries/dict_intro.py
@@ -0,0 +1,15 @@
+# Definition - Ordered collection of items
+
+# Key-value pairs
+# Values can be any data type and can repeat
+# Keys must be immutable and unique
+# dict() for creating
+# a = dict(name="Gosho", age=15)
+# print(a)
+# 6.6 times faster than lists
+# Different architecture than lists - Hash tables - Keys -> Hash code
+# Mutable objects - Key's value could be updated
+
+
+
+
