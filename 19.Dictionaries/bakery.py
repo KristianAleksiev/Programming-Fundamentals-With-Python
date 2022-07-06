849b31480b4b04dfe53ee190e7114ec3510e76fc
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-06 21:42:16 +0300

    Add files via upload

---
+++ 19.Dictionaries/bakery.py
@@ -0,0 +1,9 @@
+data = input().split()
+products_dict = {}
+
+for _ in range(0, len(data), 2):
+    products_dict[data[_]] = int(data[_+1])
+
+print(products_dict)
+
+# products_dict = {data[i]: int(data[i+1]) for i in range(0, len(data), 2)}
