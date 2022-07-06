be8333c4cb7455b082bb3056f510c317fa4a8e3c
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-06 21:43:29 +0300

    Add files via upload

---
+++ 19.Dictionaries/dict_keys_values.py
@@ -0,0 +1,10 @@
+# Keys and values
+my_dict = {
+    "fruit": "Apple",
+    "vegetable": "Cucumber",
+}
+print(my_dict)
+
+# Getting the key value
+print(my_dict["fruit"])# KeyError exception
+print(my_dict.get("fruit"))# None
