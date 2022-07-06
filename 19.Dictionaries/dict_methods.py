75994044c0ade11af8e1e9f3282a6c87ea352c07
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-06 21:44:00 +0300

    Add files via upload

---
+++ 19.Dictionaries/dict_methods.py
@@ -0,0 +1,14 @@
+# Dictionary methods
+
+my_dict = {1: "Apple", 2: "Banana"}
+my_second_dict = {3: "Pineapple", 4: "Pear"}
+
+# my_dict.clear()
+# my_dict.copy()
+# my_dict.pop(key) - Pops the key and returns its value
+# my_dict.popitem() - Returns the last element as a tuple
+# del my_dict["key"]
+# my_dict.get("key") -  Returns value of the key
+# my_dict.setdefault(3, "three") - If element not in dict, creates a new key value pair
+# my_dict.update(my_second_dict)
+# help(dict)
