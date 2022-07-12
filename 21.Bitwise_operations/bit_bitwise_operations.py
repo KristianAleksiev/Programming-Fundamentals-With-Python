4dc3a1b4e1360cc795ad58a9cf82622f1af411b4
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-12 20:01:44 +0300

    Add files via upload

---
+++ 21.Bitwise_operations/bit_bitwise_operations.py
@@ -0,0 +1,25 @@
+# Bits, Numerals systems and bitwise operations
+
+# What is Bit, Byte, KB, MB?
+# - Bit - The smallest unit of data used in computing - Takes two values - 0 or 1 ( Two states )
+# - Byte - A sequence of 8 bits, organized in computer memory, the smallest addressable unit
+# - KiloByte - 1024 bytes ( sometimes 1000 bytes - marketing )
+# - MegaByte - 1024 kb
+
+
+# Numerals systems - Representing the sequence of digits
+# - Decimal - Represented using 10 digits, base 10
+# - Binary - 0 and 1 - 5 == 101b - base 2
+# - Hexadecimal - Conversion to binary - 0-9, A-E
+# Conversion between numeral systems
+
+
+# Representation of Data in computer memory
+# - Representing integers - sequence of bits - starts with 0(+), with 1 (-)
+# - Real numbers - IEEE 754 Standard, exponent, mantissa
+# - Text - ASCII table, unsigned integers
+
+
+# Bitwise operators - bit by bit calculations - Competitive programming, code efficiency
+# - ~ Turns all 0 to 1 and all 1 to 0 ( 101- 010)
+# - |, & and ^
