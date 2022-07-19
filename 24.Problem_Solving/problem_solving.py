adba0ffae2cba6ff27b647ccccb4653b9d149065
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-19 20:24:54 +0300

    Add files via upload

---
+++ 24.Problem_Solving/problem_solving.py
@@ -0,0 +1,21 @@
+"""
+Tech problems:
+
+1. Definition
+Design, and logic implementation in order to create an app or software system
+input , output , behavior
+
+2. Goals - Implemented functionality
+
+3. Technical difficulties
+Barriers, obstacles, limitations to implement the logic
+
+
+4. How to solve it?
+- Define the problem
+- Analyse and understand the problem - if it's big, break it down into smaller ones
+- Identify solutions
+- Evaluate, try - test
+- Plan
+- Implementation
+"""
