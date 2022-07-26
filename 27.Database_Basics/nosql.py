8d55a62a0ef8744f70aded9938d3501215a39361
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-26 21:14:37 +0300

    Add files via upload

---
+++ 27.Database_Basics/nosql.py
@@ -0,0 +1,7 @@
+"""
+Unstructured database - dynamic schema for unstructured data
+Data is stored in many ways
+Horizontal scaling - More servers
+Superior performance (if needed)
+Harder to control
+"""
