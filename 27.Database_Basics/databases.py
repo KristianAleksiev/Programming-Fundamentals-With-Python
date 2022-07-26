fd2599ef60789e63a6686ae561573f4c37504a93
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-26 21:14:25 +0300

    Add files via upload

---
+++ 27.Database_Basics/databases.py
@@ -0,0 +1,15 @@
+"""
+Database - Collection of data, organized to be easily accessed, managed and updated
+Managing DBs - CRUD
+Executing queries
+
+Primary reason for a database usage - ease of search and update, security and access control
+
+SQL vs NoSQL - General purpose databases
+
+Relational - Hold data in tables + relationships (SQL - Structured Query Language)
+MySQL, PostgreSQL, Oracle, SQLite
+
+NoSQL - Collections of documents (key-value)
+MongoDB, Redis, IndexedDB, Cassandra
+"""
