5d80ace61e6445e085d2507aa8f345db46e8ae7e
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-26 21:14:57 +0300

    Add files via upload

---
+++ 27.Database_Basics/sql.py
@@ -0,0 +1,13 @@
+"""
+Every table is pre-defined as a structure - columns with data types
+Relations between different tables
+Most used data management technology
+Organizes the data and linking it by a unique key and a foreign key
+Very scalable - Increasing the load on a server -> Increase CPU, RAM
+The table is the main building block
+
+SELECT * from table
+SELECT * from table WHERE x = y
+
+RDBMS - MySQL, PostgreSQL
+"""
