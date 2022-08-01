94f84959fffe037401b03c83dcd2e7c7dd69bdfc
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-01 21:39:06 +0300

    Add files via upload

---
+++ 29.QA_Introduction/quality_assurance.py
@@ -0,0 +1,37 @@
+"""
+1. Software Quality Assurance - Introduction
+- Bug free software
+- Part of SDLC
+
+2. QA Engineers and responsibilities
+- Ensures the software quality
+- Software manual and automated testing for proper implementation of business logic
+- Create, design and executes tests
+- Report and track bugs
+- Performs regression testing
+
+3. Bugs and bug trackers
+- Defect vs failure
+- Importance of finding defects earlier in SDLC
+- Bug tracking systems - title and description - state - status - priority
+
+4. Testing, test types, test levels
+Types:
+- Requirements testing
+- Static testing
+- Dynamic testing - Functional, non-functional testing
+- Non-functional testing - Stress tests, load tests, usability tests, security tests
+Levels:
+- Unit tests - a single component
+- Integration tests - interaction between components
+- System tests - entire system
+
+5. Test automation, frameworks and tools
+Frameworks - JUInit, NUnit, Mocha
+Automated testing tools - Selenium, Appium
+
+6. Continuous integration and continuous delivery (CI/CD).
+Continuous delivery/Continuous integration pipeline - Continuously integrate and release new features
+Continuous integration - Test and integrate code in the product
+Continuous delivery - Release of new features
+"""
